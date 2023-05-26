3306063be49a3e62637980761785007644bd65eb
mohamedwazeem <110921013+mohamedwazeem@users.noreply.github.com>
2023-05-26 12:31:12 +0530

    Add files via upload

---
+++ datasource/ethDatasource.js
@@ -0,0 +1,36 @@
+require("dotenv").config();
+
+const { RESTDataSource } = require("apollo-datasource-rest");
+
+//Vitalik's Ethereum Address
+const eth_address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045";
+
+//Etherscan Data Source Class
+class EtherDataSource extends RESTDataSource {
+  constructor() {
+    super();
+    this.baseURL = "https://api.etherscan.io/api";
+  }
+
+  async etherBalanceByAddress() {
+    const response = await this.get(this.baseURL, {
+      module: "account",
+      action: "balance",
+      address: eth_address,
+      tag: "latest",
+      apikey: process.env.ETHERSCAN_API,
+    });
+    return response;
+  }
+
+  async totalSupplyOfEther() {
+    const response = await this.get(this.baseURL, {
+      module: "stats",
+      action: "ethsupply",
+      apikey: process.env.ETHERSCAN_API,
+    });
+    return response;
+  }
+}
+
+module.exports = EtherDataSource;
